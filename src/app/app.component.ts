fe6d712d7ad8030aa279002fe8fbb8a62a205a90
Mateus Bazani <mrbazani@gmail.com>
2019-05-30 11:17:40 -0300

    Adicionado bootstrap. Criados serviços para consultar as APIs. Criadas validações báscias.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'climadex';
+  dadosInvalidos = false;
+  nomeCidade = '';
+
+  pesquisar() {
+    
+    console.log('ueeeeee', this.nomeCidade);
+  }
 }
